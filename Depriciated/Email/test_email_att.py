40c0b9f5407341b35c06c46d450688db0b676ebf
NotAun <shaikhaunraza@gmail.com>
2026-03-09 04:21:52 +0530

    Add files via upload

---
+++ Depriciated/Email/test_email_att.py
@@ -0,0 +1,8 @@
+from email_handler import send_email
+
+send_email(
+    to_email="nidaqureshin04@gmail.com",
+    subject="Attachment Test",
+    message_text="File attached",
+    attachments=["C:\\Users\\Nida\\OneDrive\\Desktop\\Prova_attachment.txt"]
+)
